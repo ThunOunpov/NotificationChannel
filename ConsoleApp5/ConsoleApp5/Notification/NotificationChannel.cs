1c82c9160bf9b83b61caabe48a4c60642a94fc32
ThunOunpov <117258711+ThunOunpov@users.noreply.github.com>
2023-10-06 09:43:46 +0700

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Notification/NotificationChannel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification.Notification
+{
+    public class NotificationChannel
+    {
+        public string Name { get; set; }
+        public List<NotificationTitle> NotificationTitles { get; set; }
+        public List<NotificationChannel> NotificationChannels { get; set; }
+        public List<string> Channeltypes = new List<string>();
+        public NotificationChannel()
+        {
+            NotificationTitles = new List<NotificationTitle>();
+            Channeltypes = new List<string>();
+            NotificationChannels= new List<NotificationChannel>();
+            // add notification channel.
+            Channeltypes.Add(NoficationChanneType.BE);
+            Channeltypes.Add(NoficationChanneType.FE);
+            Channeltypes.Add(NoficationChanneType.QA);
+            Channeltypes.Add(NoficationChanneType.Urgent);
+
+        }
+    }
+    public class NoficationChanneType
+    {
+        public const string BE = "BE";
+        public const string FE = "FE";
+        public const string QA = "QA";
+        public const string Urgent = "Urgent";
+
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Notification.Notification;
+
+namespace Notification
+{
+    class Program
+    {
+
+        static NotificationChannel channel = new NotificationChannel();
+        static List<string> channelTypes = channel.Channeltypes;
+        static void Main(string[] args)
+        {
+
+            // add notification channel.
+            channelTypes.ForEach(x => channel.NotificationChannels.Add(new NotificationChannel { Name = x }));
+
+        Start:
+            Console.WriteLine("Please input notification title:");
+            string notificationTitle = Console.ReadLine();
+
+            // Parse the notification title and get the list of tags.
+            List<string> tags = ParseNotificationTitle(notificationTitle);
+
+            // display message error the console.
+            string messageError = "Output receive channels:";
+            string text = string.Empty;
+            List<NotificationChannel> channels = channel.NotificationChannels.Where(w => tags.Where(x => x == w.Name).Any()).ToList();
+            foreach (NotificationChannel notificationChannel in channels)
+            {
+                text += $" {notificationChannel.Name},";
+            }
+            if (!string.IsNullOrEmpty(text))
+                text = text.Remove(text.Length - 1, 1);
+            Console.WriteLine(messageError + text);
+        Decision:
+            Console.WriteLine(format: "Do you want test other case - Yes/No?");
+            string userDecision = Console.ReadLine();
+            switch (userDecision.ToUpper())
+            {
+                case "YES":
+                    goto Start;
+                case "NO":
+                    break;
+                default:
+                    goto Decision;
+            }
+            Console.ReadKey();
+        }
+
+        static List<string> ParseNotificationTitle(string notificationTitle)
+        {
+            List<string> tags = new List<string>();
+            char[] spearator = { '[', ']', ' ' };
+
+            string[] splitTitle = notificationTitle.Split(spearator);
+
+            foreach (string tag in splitTitle)
+            {
+                tags.Add(tag.Trim());
+            }
+            // Remove any empty strings from the list.
+            tags.RemoveAll(string.IsNullOrEmpty);
+
+            return tags;
+        }
+
+    }
+}
